baa65600aa89089df169d8377caf740d95b225cf
yaponec432-hashu <234154007+yaponec432-hashu@users.noreply.github.com>
2026-09-25 01:45:36 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,30 +1,51 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 # SPDX-License-Identifier: 0BSD
 # Download music
-set -e -o pipefail
+set -eu -o pipefail
 
-FILTER='acompressor=threshold=0.6:ratio=4:link=maximum,bass=g=6'
-FILTER="${FILTER},treble=f=12000:g=6,equalizer=f=3500:t=h:w=3000:g=-10"
-FILTER="${FILTER},volume=0.5,alimiter=limit=0.95"
+FILTER='bass=g=6,equalizer=f=3500:t=h:w=3000:g=-10,crossfeed,loudnorm'
+FILTER="${FILTER}=stats_file=-:print_format=json"
+TEMP_FILE="${TMPDIR}/google-music-temp.m4a"
 
 download() {
-    local file_name="${1}"
-    local link="${2}"
+    local file_name="${1/,*}"
+    local link="${1#*,}"
 
     [[ -f "${file_name}.ogg" ]] && return
-    yt-dlp "${link}" -f 251/140/ba --cookies "${HOME}/cookies" -r 1M \
-        --min-sleep-interval 15 --max-sleep-interval 30 --xff jp -o - \
-        | ffmpeg -v 24 -stats -y -i - -vn -c:a libopus -vbr on -b:a 128k \
-        -af "${FILTER}" -- "./${file_name}.ogg"
+    echo "Downloading ${file_name}"
+    yt-dlp "${link}" -f 140 --cookies "${HOME}/cookies" \
+        --min-sleep-interval 3 --max-sleep-interval 5 --embed-metadata \
+        --force-overwrites -o "${TEMP_FILE}"
+
+    local json
+    json="$(ffmpeg -v 24 -stats -i ${TEMP_FILE} -vn -af ${FILTER} \
+        -f null -- - \
+        | sed -n '/{/,/}/p' | tr -d '":,' | awk '{print $2}')"
+
+    local i
+    local tp
+    local lra
+    local thresh
+    i=$(echo "${json}" | sed -n 2p)
+    tp=$(echo "${json}" | sed -n 3p)
+    lra=$(echo "${json}" | sed -n 4p)
+    thresh=$(echo "${json}" | sed -n 5p)
+
+    local measured
+    measured="${FILTER}:measured_i=${i}:measured_tp=${tp}:measured_lra=${lra}"
+    measured="${measured}:measured_thresh=${thresh}"
+
+    ffmpeg -v 24 -stats -y -i "${TEMP_FILE}" -vn -c:a aac -b:a 128k \
+        -af "${measured}" -- "./${file_name}.m4a"
 }
 
 main() {
-    chmod 600 "${HOME}/cookies"
     [[ -n "${1}" ]] && yt-dlp --update-to nightly
-    local file_name
-    local link
-    tail -n +2 download-list.csv | while IFS=',' read -r file_name link; do
-        download "${file_name}" "${link}"
+    local list
+    list="$(tail -n +2 ./download-list.csv)"
+    local song
+    for song in ${list}; do
+        download "${song}"
     done
 }
 
