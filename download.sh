07b18545da73879657983b00e1e3e1fced1fd54b
yaponec432-hashu <234154007+yaponec432-hashu@users.noreply.github.com>
2026-09-26 21:01:40 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,9 +2,10 @@
 # SPDX-License-Identifier: 0BSD
 # Download the music
 
-FILTER='crossfeed,virtualbass,pan=stereo|FL=FL+LFE|FR=FR+LFE'
-FILTER="${FILTER},equalizer=f=3500:t=h:w=3000:g=-10,highpass=f=80"
-FILTER="${FILTER},alimiter=limit=0.95,dynaudnorm=f=8000:p=0.1"
+# Reduce the ear fatigue
+FILTER='virtualbass,pan=stereo|FL=FL+LFE|FR=FR+LFE,equalizer=f=3500:t=h:w=3000'
+FILTER="${FILTER}:g=-10,highpass=f=80,crossfeed,alimiter=limit=0.95"
+FILTER="${FILTER},loudnorm=lra=50:tp=-9:i=-28"
 
 download() {
     local file_name="${1/,*}"
@@ -19,9 +20,22 @@ download() {
         --min-sleep-interval 1 --max-sleep-interval 3 --embed-metadata \
         --force-overwrites -o "${temp_file}"
 
-    ffmpeg -v 24 -stats -y -i "${temp_file}" -vn -c:a aac \
-        -movflags +faststart+use_metadata_tags -af "${FILTER}" \
-        -- "${output_file}"
+    local data
+    data="$(ffmpeg -hide_banner -i ${temp_file} \
+        -af ebur128=framelog=quiet:peak=true -f null - 2>&1)"
+
+    local i tp lra threshold
+    i=$(echo "${data}" | grep 'I: .* LUFS$' | awk '{print $2}')
+    tp=$(echo "${data}" | grep 'Peak: .* dBFS$' | awk '{print $2}')
+    lra=$(echo "${data}" | grep 'LRA: .* LU$' | awk '{print $2}')
+    threshold=$(echo "${data}" | grep 'Threshold: .* LUFS$' | awk '{print $2}')
+
+    local filter
+    filter="${FILTER}:measured_I=${i}:measured_TP=${tp}:measured_LRA=${lra}"
+    filter="${filter}:measured_thresh=${threshold}"
+
+    ffmpeg -v 24 -stats -y -i "${temp_file}" -vn -c:a aac -b:a 128k \
+        -af "${filter}" -ar 44100 -- "${output_file}"
 }
 
 main() {
